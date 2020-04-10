69b30d686ac4fb26edd866c6efd7e297470d0be9
ludwj61 <ludwj61@mail.sunysuffolk.edu>
2020-04-10 18:01:46 -0400

    test adding things to table

--- to do list/script.js
+++ to do list/script.js
@@ -1,1 +1,10 @@
-const table = document.querySelector("table")
+const table = document.querySelector("table");
+
+for (var i = 0; i < 20; i++) {
+  let row = table.insertRow(1);
+  let cell = row.insertCell(0);
+  let cell2 = row.insertCell(1);
+
+  cell.innerHTML = "Do something";
+  cell2.innerHTML = "Do something else";
+}
