7c2095405f8449a0e67096ca064b839314989aa9
ludwj61 <ludwj61@mail.sunysuffolk.edu>
2020-04-11 13:14:41 -0400

    fin example but not working locally because of free api plan

--- flask example/app.py
+++ flask example/app.py
@@ -7,3 +7,19 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/convert", methods=["POST"])
+def convert():
+    currency = request.form.get("currency")
+    res = requests.get("http://data.fixer.io/api/latest?access_key=314dd5e57ab376e68890c481801a47d7",
+                       params={"base": "USD", "symbols": currency})
+
+    if res.status_code != 200:
+        return jsonify({"success": False})
+
+    data = res.json()
+    if currency not in data["rates"]:
+        return jsonify({"success": False})
+
+    return jsonify({"success": True, "rate": data["rates"][currency]})
---
+++ flask example/static/index.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelector('#form').onsubmit = () => {
+    const request = new XMLHttpRequest();
+    const currency = document.querySelector('#currency').value;
+    request.open('POST', '/convert');
+
+    request.onload = () => {
+      const data = JSON.parse(request.responseText);
+      if (data.success) {
+        const contents = `1 USD is equal to ${data.rate} ${currency}.`
+        document.querySelector('#result').innerHTML = content;
+      } else {
+        document.querySelector('#result').innerHTML = 'There was an error converting.'
+      }
+    }
+
+    const data = new FormData();
+    data.append('currency', currency);
+
+    request.send(data);
+    return false;
+  }
+})
